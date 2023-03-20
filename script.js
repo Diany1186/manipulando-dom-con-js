5ad1d7282867ae918fefce2a1a10949a3e603d37
DianyLocal <diany1186@hotmail.com>
2023-03-20 14:45:36 -0500

    crando los elementos de la lista con appendChild

--- script.js
+++ script.js
@@ -8,13 +8,21 @@ const createTask = (evento) => {
     const task = document.createElement("li");
     task.classList.add("card");
     input.value = "";
-    const content = `<div>
-        <i class="far fa-check-square icon"></i>
-        <span class="task">${value}</span>
-    </div>
-    <i class="fas fa-trash-alt trashIcon icon"></i>`;
-    task.innerHTML = content;
     
+    console.log(checkComplete());
+    const taskContent = document.createElement("div");
+    taskContent.appendChild(checkComplete());
+    const titleTask = document.createElement("span");
+    titleTask.classList.add("task");
+    titleTask.innerText = value;
+    taskContent.appendChild(titleTask);
+    
+    const content = `
+        
+    <i class="fas fa-trash-alt trashIcon icon"></i>`;
+    
+    //task.innerHTML = content;
+    task.appendChild(taskContent)
     list.appendChild(task);
 
     console.log(content);
@@ -23,3 +31,11 @@ const createTask = (evento) => {
 console.log(btn);
 
 btn.addEventListener("click", createTask);
+
+const checkComplete = () => {
+    const i = document.createElement("i");
+    i.classList. add("far");
+    i.classList. add("fa-check-square");
+    i.classList. add("icon");
+    return i;
+}
